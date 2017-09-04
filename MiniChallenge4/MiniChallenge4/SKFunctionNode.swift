36f32cd7fe95078d17d8fd0306e74d5d9df92da7
guilhermePaciulli <gpaciulli100@gmail.com>
2017-09-04 14:48:08 -0300

    Initial modeling

---
+++ MiniChallenge4/MiniChallenge4/SKFunctionNode.swift
@@ -0,0 +1,11 @@
+//
+//  SKFunctionNode.swift
+//  MiniChallenge4
+//
+//  Created by Guilherme Paciulli on 04/09/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
