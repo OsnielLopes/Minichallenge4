edf039b2e62ab831e30469ed71d7f6a6d191302a
guilhermePaciulli <gpaciulli100@gmail.com>
2017-09-04 14:15:19 -0300

    Project configuration

---
+++ MiniChallenge4/MiniChallenge4/GameScene.swift
@@ -0,0 +1,42 @@
+//
+//  GameScene.swift
+//  MiniChallenge4
+//
+//  Created by Guilherme Paciulli on 04/09/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
---
+++ MiniChallenge4/MiniChallenge4/GameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  GameViewController.swift
+//  MiniChallenge4
+//
+//  Created by Guilherme Paciulli on 04/09/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            if let scene = SKScene() as? GameScene {
+                scene.scaleMode = .aspectFill
+                view.presentScene(scene)
+            }
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
