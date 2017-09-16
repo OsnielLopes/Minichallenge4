ca4bd17e66b0b5a1e2f4f32e8b700471cd924348
guilhermePaciulli <gpaciulli100@gmail.com>
2017-09-16 13:47:59 -0300

    Sound implementation

---
+++ MiniChallenge4/MiniChallenge4/SoundManager.swift
@@ -0,0 +1,13 @@
+//
+//  SoundManager.swift
+//  MiniChallenge4
+//
+//  Created by Guilherme Paciulli on 16/09/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
+
+struct Audios {
+    static var MAIN_THEME = "main_theme.mp3"
+}
