f34b4d25b8ca1f8445b2986b4f96767aea4dbc9e
OsnielLopes <niellt@hotmail.com>
2017-09-17 16:27:41 -0300

    Developers images with rounded corners

--- MiniChallenge4/MiniChallenge4/AboutViewController.swift
+++ MiniChallenge4/MiniChallenge4/AboutViewController.swift
@@ -13,9 +13,21 @@ class AboutViewController: UIViewController {
     
     var backToMainMenu: AVAudioPlayer!
 
+    @IBOutlet weak var chevisImage: UIImageView!
+    @IBOutlet weak var paciulliImage: UIImageView!
+    @IBOutlet weak var osnielImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backToMainMenu = Audio.playSound(named: Audio.CLICK_OUT)
+        
+        chevisImage.layer.cornerRadius = 8.0
+        chevisImage.clipsToBounds = true
+        paciulliImage.layer.cornerRadius = 8.0
+        paciulliImage.clipsToBounds = true
+        osnielImage.layer.cornerRadius = 8.0
+        osnielImage.clipsToBounds = true
     }
 
     @IBAction func back(_ sender: Any) {
